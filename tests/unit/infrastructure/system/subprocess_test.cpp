8e6b6852c214208e0786e7dbbed63c45988f4b18
Stanes Wilson <staneswilson2006@gmail.com>
2026-06-23 09:36:26 +0000

    style: satisfy clang-format in subprocess tests

--- tests/unit/infrastructure/system/subprocess_test.cpp
+++ tests/unit/infrastructure/system/subprocess_test.cpp
@@ -17,7 +17,6 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers.hpp>
 #include <catch2/matchers/catch_matchers_string.hpp>
-
 #include <cmlb/core/error.hpp>
 #include <cmlb/infrastructure/system/subprocess.hpp>
 
